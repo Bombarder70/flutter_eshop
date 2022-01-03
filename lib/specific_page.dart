bf047cae5b707323c42741efcde48585f65f5155
Bombarder70 <holespato@gmail.com>
2022-01-03 19:43:24 +0100

    PoructCard

--- lib/specific_page.dart
+++ lib/specific_page.dart
@@ -14,7 +14,156 @@ class SecondPageState extends State<SecondPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: ListView(),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              children: [
+                FilterItem(title: "Zoradiť", icon: Icons.sort),
+                FilterItem(title: "Filtrovať", icon: Icons.filter_alt),
+              ],
+            ),
+          ),
+          Divider(color: Colors.black),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        child:
+                            Image.network('https://picsum.photos/250?image=9'),
+                      ),
+                      Text(
+                        "Name of product",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        "560€",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        "-50%",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset:
+                            const Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  child: Column(
+                    children: [
+                      Image.network('https://picsum.photos/250?image=9'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FilterItem extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const FilterItem({
+    Key? key,
+    required this.title,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: Colors.grey,
+            size: 30.0,
+          ),
+          const VerticalDivider(
+            color: Colors.black,
+            thickness: 2,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 5),
+            child: Text(
+              title,
+              style: const TextStyle(fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const ProductCard({
+    Key? key,
+    required this.title,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: Colors.grey,
+            size: 30.0,
+          ),
+          const VerticalDivider(
+            color: Colors.black,
+            thickness: 2,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 5),
+            child: Text(
+              title,
+              style: const TextStyle(fontSize: 20),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
