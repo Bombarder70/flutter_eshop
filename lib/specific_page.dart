aaf9fc3dde42add37ce705b94dd4a352e5979120
Bombarder70 <holespato@gmail.com>
2022-01-03 19:52:00 +0100

    ProductCard Stateless

--- lib/specific_page.dart
+++ lib/specific_page.dart
@@ -28,62 +28,14 @@ class SecondPageState extends State<SecondPage> {
           Divider(color: Colors.black),
           Row(
             children: [
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child:
-                            Image.network('https://picsum.photos/250?image=9'),
-                      ),
-                      Text(
-                        "Name of product",
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      Text(
-                        "560€",
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      Text(
-                        "-50%",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.green,
-                        ),
-                      ),
-                    ],
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset:
-                            const Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(5),
-                  child: Column(
-                    children: [
-                      Image.network('https://picsum.photos/250?image=9'),
-                    ],
-                  ),
-                ),
-              ),
+              ProductCard(title: "xxx"),
+              ProductCard(title: "xxx"),
+            ],
+          ),
+          Row(
+            children: [
+              ProductCard(title: "xxx"),
+              ProductCard(title: "xxx"),
             ],
           )
         ],
@@ -132,37 +84,56 @@ class FilterItem extends StatelessWidget {
 
 class ProductCard extends StatelessWidget {
   final String title;
-  final IconData icon;
 
   const ProductCard({
     Key? key,
     required this.title,
-    required this.icon,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            icon,
-            color: Colors.grey,
-            size: 30.0,
-          ),
-          const VerticalDivider(
-            color: Colors.black,
-            thickness: 2,
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 5),
-            child: Text(
-              title,
-              style: const TextStyle(fontSize: 20),
+      child: Container(
+        margin: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Image.network('https://picsum.photos/250?image=9'),
             ),
-          ),
-        ],
+            Text(
+              "Name of product",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              "560€",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            Text(
+              "-50%",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
       ),
     );
   }
