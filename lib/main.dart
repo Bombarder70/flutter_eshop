286a3120dd53a27937bda2964a32990c8b0f8025
Bombarder70 <holespato@gmail.com>
2022-01-03 15:13:19 +0100

    Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -40,135 +40,72 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        body: ListView(children: [
-          Row(
-            children: [
-              HeaderCard(),
-              HeaderCard(),
-              HeaderCard(),
-            ],
-          ),
-          Container(
-            color: Colors.red,
-            height: 175,
-            margin: EdgeInsets.only(bottom: 10),
-          ),
-          Row(
-            children: [
-              AllCategoriesCard(),
-              AllCategoriesCard(),
-              AllCategoriesCard(),
-            ],
-          ),
-          Row(
-            children: [
-              AllCategoriesCard(),
-              AllCategoriesCard(),
-              AllCategoriesCard(),
-            ],
-          ),
-          Row(
-            children: [
-              AllCategoriesCard(),
-              AllCategoriesCard(),
-              AllCategoriesCard(),
-            ],
-          ),
-        ]),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        body: ListView(
+          children: [
+            Row(
+              children: [
+                HeaderCard(title: "Men"),
+                HeaderCard(title: "Woman"),
+                HeaderCard(title: "Childs"),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              color: Colors.red,
+              height: 175,
+              margin: EdgeInsets.only(bottom: 10),
+            ),
+            Row(
+              children: [
+                AllCategoriesCard(),
+                AllCategoriesCard(),
+                AllCategoriesCard(),
+              ],
+            ),
+            Row(
+              children: [
+                AllCategoriesCard(),
+                AllCategoriesCard(),
+                AllCategoriesCard(),
+              ],
+            ),
+            Row(
+              children: [
+                AllCategoriesCard(),
+                AllCategoriesCard(),
+                AllCategoriesCard(),
+              ],
             ),
           ],
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange[200],
+                ),
+                child: const Text('Drawer Header'),
+              ),
+              const DrawerItem(title: "Woman"),
+              const DrawerItem(title: "Men"),
+              const DrawerItem(title: "Childs"),
+              const Divider(
+                color: Colors.black,
+                height: 25,
+              )
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
 class HeaderCard extends StatelessWidget {
-  const HeaderCard({
-    Key? key,
-  }) : super(key: key);
+  final String title;
+
+  const HeaderCard({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -190,7 +127,7 @@ class HeaderCard extends StatelessWidget {
               ),
             ),
             Text(
-              "Woman",
+              title,
               style: TextStyle(fontSize: 17),
             ),
           ],
@@ -209,11 +146,33 @@ class AllCategoriesCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        padding: EdgeInsets.all(5),
-        margin: EdgeInsets.all(5),
+        padding: const EdgeInsets.all(5),
+        margin: const EdgeInsets.all(5),
         color: Colors.deepOrange[200],
         child: Image.network('https://picsum.photos/250?image=9'),
       ),
+    );
+  }
+}
+
+class DrawerItem extends StatelessWidget {
+  final String title;
+
+  const DrawerItem({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        title,
+        style: const TextStyle(fontSize: 18),
+      ),
+      onTap: () {
+        Navigator.pop(context);
+      },
     );
   }
 }
